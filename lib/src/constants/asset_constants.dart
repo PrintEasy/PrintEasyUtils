7ffaffce9a881745ba436292fcc0b81bd104830f
Jatin <contact.dev.jatin@gmail.com>
2024-07-22 00:32:27 +0530

    fix: remove packages prefix

--- lib/src/constants/asset_constants.dart
+++ lib/src/constants/asset_constants.dart
@@ -1,28 +1,17 @@
-import 'package:printeasy_utils/printeasy_utils.dart';
-
 class AssetConstants {
   const AssetConstants._();
 
-  static const String _logo = 'assets/images/logo.png';
-  static const String logo = 'packages/${PrintEasyConstants.appName}/$_logo';
+  static const String logo = 'assets/images/logo.png';
 
-  static const String _helpCenter = 'assets/svg/help_center.svg';
-  static const String helpCenter = 'packages/${PrintEasyConstants.appName}/$_helpCenter';
+  static const String helpCenter = 'assets/svg/help_center.svg';
 
-  static const String _termsCondition = 'assets/svg/terms_conditions.svg';
-  static const String termsCondition = 'packages/${PrintEasyConstants.appName}/$_termsCondition';
-  static const String _privacyPolicy = 'assets/svg/privacy_policy.svg';
-  static const String privacyPolicy = 'packages/${PrintEasyConstants.appName}/$_privacyPolicy';
-  static const String _refund = 'assets/svg/refund.svg';
-  static const String refund = 'packages/${PrintEasyConstants.appName}/$_refund';
-  static const String _shipping = 'assets/svg/shipping.svg';
-  static const String shipping = 'packages/${PrintEasyConstants.appName}/$_shipping';
+  static const String termsCondition = 'assets/svg/terms_conditions.svg';
+  static const String privacyPolicy = 'assets/svg/privacy_policy.svg';
+  static const String refund = 'assets/svg/refund.svg';
+  static const String shipping = 'assets/svg/shipping.svg';
 
-  static const String _whatsapp = 'assets/svg/whatsapp.svg';
-  static const String whatsapp = 'packages/${PrintEasyConstants.appName}/$_whatsapp';
-  static const String _google = 'assets/svg/google.svg';
-  static const String google = 'packages/${PrintEasyConstants.appName}/$_google';
+  static const String whatsapp = 'assets/svg/whatsapp.svg';
+  static const String google = 'assets/svg/google.svg';
 
-  static const String _spiral = 'assets/svg/spiral.svg';
-  static const String spiral = 'packages/${PrintEasyConstants.appName}/$_spiral';
+  static const String spiral = 'assets/svg/spiral.svg';
 }
