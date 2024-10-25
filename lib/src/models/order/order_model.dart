e4bf893a1387be91bf46f31c1092bbbea9aaa0cb
Jatin <contact.dev.jatin@gmail.com>
2024-10-25 12:18:48 +0530

    fix: remove collection ref from order model

--- lib/src/models/order/order_model.dart
+++ lib/src/models/order/order_model.dart
@@ -9,14 +9,11 @@ class OrderModel {
     this.shipmentId,
     this.razorpay,
     required this.subcategory,
-    required this.userRef,
     this.user,
     required this.totalAmount,
     this.orderDate,
     this.status = OrderStatus.paymentPending,
     required this.items,
-    required this.shippingAddressRef,
-    required this.billingAddressRef,
     this.shippingAddressId,
     this.billingAddressId,
     this.shippingAddress,
@@ -29,10 +26,6 @@ class OrderModel {
       shipmentId: map['shipmentId'] as String?,
       razorpay: map['razorpay'] != null ? RazorpayPaymentModel.fromMap(map['razorpay']) : null,
       subcategory: Subcategory.fromName(map['subcategory'] as String? ?? ''),
-      userRef: (map['userRef'] as DocumentReference).withConverter<UserModel>(
-        fromFirestore: (snapshot, _) => UserModel.fromMap(snapshot.data()!),
-        toFirestore: (data, _) => data.toMap(),
-      ),
       user: map['user'] != null ? UserModel.fromMap(map['user'] as Map<String, dynamic>) : null,
       totalAmount: map['totalAmount'] as double,
       orderDate: map['orderDate'] != null ? (map['orderDate'] as Timestamp).toDate() : null,
@@ -42,14 +35,6 @@ class OrderModel {
             (x) => ItemModel.fromMap(x as Map<String, dynamic>),
           )
           .toList(),
-      shippingAddressRef: (map['shippingAddressRef'] as DocumentReference).withConverter<AddressModel>(
-        fromFirestore: (snapshot, _) => AddressModel.fromMap(snapshot.data()!),
-        toFirestore: (data, _) => data.toMap(),
-      ),
-      billingAddressRef: (map['billingAddressRef'] as DocumentReference).withConverter<AddressModel>(
-        fromFirestore: (snapshot, _) => AddressModel.fromMap(snapshot.data()!),
-        toFirestore: (data, _) => data.toMap(),
-      ),
       shippingAddressId: map['shippingAddressId'] as String?,
       billingAddressId: map['billingAddressId'] as String?,
       shippingAddress: map['shippingAddress'] != null ? AddressModel.fromMap(map['shippingAddress'] as Map<String, dynamic>) : null,
@@ -69,17 +54,14 @@ class OrderModel {
   final String? shipmentId;
   final Subcategory subcategory;
   final RazorpayPaymentModel? razorpay;
-  final DocumentReference<UserModel> userRef;
   final UserModel? user;
   final double totalAmount;
   final DateTime? orderDate;
   final OrderStatus status;
   final List<ItemModel> items;
   final String? shippingAddressId;
-  final DocumentReference<AddressModel> shippingAddressRef;
   final AddressModel? shippingAddress;
   final String? billingAddressId;
-  final DocumentReference<AddressModel> billingAddressRef;
   final AddressModel? billingAddress;
 
   String get mobile => user?.phone ?? '';
@@ -89,14 +71,11 @@ class OrderModel {
     String? shipmentId,
     RazorpayPaymentModel? razorpay,
     Subcategory? subcategory,
-    DocumentReference<UserModel>? userRef,
     UserModel? user,
     double? totalAmount,
     DateTime? orderDate,
     OrderStatus? status,
     List<ItemModel>? items,
-    DocumentReference<AddressModel>? shippingAddressRef,
-    DocumentReference<AddressModel>? billingAddressRef,
     String? shippingAddressId,
     String? billingAddressId,
     AddressModel? shippingAddress,
@@ -107,14 +86,11 @@ class OrderModel {
         shipmentId: shipmentId ?? this.shipmentId,
         razorpay: razorpay ?? this.razorpay,
         subcategory: subcategory ?? this.subcategory,
-        userRef: userRef ?? this.userRef,
         user: user ?? this.user,
         totalAmount: totalAmount ?? this.totalAmount,
         orderDate: orderDate ?? this.orderDate,
         status: status ?? this.status,
         items: items ?? this.items,
-        shippingAddressRef: shippingAddressRef ?? this.shippingAddressRef,
-        billingAddressRef: billingAddressRef ?? this.billingAddressRef,
         shippingAddressId: shippingAddressId ?? this.shippingAddressId,
         billingAddressId: billingAddressId ?? this.billingAddressId,
         shippingAddress: shippingAddress ?? this.shippingAddress,
@@ -126,14 +102,11 @@ class OrderModel {
         'shipmentId': shipmentId,
         'razorpay': razorpay?.toMap(),
         'subcategory': subcategory.name,
-        'userRef': userRef,
         'user': user?.toMap(),
         'totalAmount': totalAmount,
         'orderDate': orderDate != null ? Timestamp.fromDate(orderDate!) : FieldValue.serverTimestamp(),
         'status': status.name,
         'items': items.map((x) => x.toMap()).toList(),
-        'shippingAddressRef': shippingAddressRef,
-        'billingAddressRef': billingAddressRef,
         'shippingAddressId': shippingAddressId,
         'billingAddressId': billingAddressId,
         'shippingAddress': shippingAddress?.toMap(),
@@ -144,7 +117,7 @@ class OrderModel {
 
   @override
   String toString() =>
-      'OrderModel(orderId: $orderId, shipmentId: $shipmentId, razorpay: $razorpay, userRef: $userRef, user: $user, subcategory: $subcategory, totalAmount: $totalAmount, orderDate: $orderDate, status: $status, items: $items, shippingAddressRef: $shippingAddressRef, billingAddressRef: $billingAddressRef, shipmentAddressId: $shippingAddressId, billingAddressId: $billingAddressId, shippingAddress: $shippingAddress, billingAddress: $billingAddress)';
+      'OrderModel(orderId: $orderId, shipmentId: $shipmentId, razorpay: $razorpay, user: $user, subcategory: $subcategory, totalAmount: $totalAmount, orderDate: $orderDate, status: $status, items: $items, shipmentAddressId: $shippingAddressId, billingAddressId: $billingAddressId, shippingAddress: $shippingAddress, billingAddress: $billingAddress)';
 
   @override
   bool operator ==(covariant OrderModel other) {
