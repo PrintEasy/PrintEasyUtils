a60060a5d13c2720963a82cf519a282d091fe07a
Jatin-Minimalist <jatin@minimalistinstitute.com>
2025-03-26 03:24:50 +0530

    fix: collection model

--- lib/src/models/category/collection_model.dart
+++ lib/src/models/category/collection_model.dart
@@ -4,6 +4,8 @@ import 'package:flutter/foundation.dart';
 
 class CollectionModel {
   const CollectionModel({
+    this.id = '',
+    this.categoryId = '',
     this.name = '',
     this.description = '',
     this.image = '',
@@ -12,10 +14,12 @@ class CollectionModel {
   });
 
   factory CollectionModel.fromMap(Map<String, dynamic> map) => CollectionModel(
-        name: map['name'] as String,
-        description: map['description'] as String,
-        image: map['image'] as String,
-        slug: map['slug'] as String,
+        id: map['id'] as String? ?? '',
+        categoryId: map['categoryId'] as String? ?? '',
+        name: map['name'] as String? ?? '',
+        description: map['description'] as String? ?? '',
+        image: map['image'] as String? ?? '',
+        slug: map['slug'] as String? ?? '',
         products: (map['products'] as List? ?? []).cast<String>(),
       );
 
@@ -26,6 +30,8 @@ class CollectionModel {
         json.decode(source) as Map<String, dynamic>,
       );
 
+  final String id;
+  final String categoryId;
   final String name;
   final String description;
   final String image;
@@ -33,6 +39,8 @@ class CollectionModel {
   final List<String> products;
 
   CollectionModel copyWith({
+    String? id,
+    String? categoryId,
     String? name,
     String? description,
     String? image,
@@ -40,6 +48,8 @@ class CollectionModel {
     List<String>? products,
   }) =>
       CollectionModel(
+        id: id ?? this.id,
+        categoryId: categoryId ?? this.categoryId,
         name: name ?? this.name,
         description: description ?? this.description,
         image: image ?? this.image,
@@ -48,6 +58,8 @@ class CollectionModel {
       );
 
   Map<String, dynamic> toMap() => <String, dynamic>{
+        'id': id,
+        'categoryId': categoryId,
         'name': name,
         'description': description,
         'image': image,
@@ -58,13 +70,16 @@ class CollectionModel {
   String toJson() => json.encode(toMap());
 
   @override
-  String toString() => 'CollectionModel(name: $name, description: $description, image: $image, slug: $slug, products: $products)';
+  String toString() =>
+      'CollectionModel(id: $id, categoryId: $categoryId, name: $name, description: $description, image: $image, slug: $slug, products: $products)';
 
   @override
   bool operator ==(covariant CollectionModel other) {
     if (identical(this, other)) return true;
 
-    return other.name == name &&
+    return other.id == id &&
+        other.categoryId == categoryId &&
+        other.name == name &&
         other.description == description &&
         other.image == image &&
         other.slug == slug &&
@@ -72,5 +87,5 @@ class CollectionModel {
   }
 
   @override
-  int get hashCode => name.hashCode ^ description.hashCode ^ image.hashCode ^ slug.hashCode ^ products.hashCode;
+  int get hashCode => id.hashCode ^ categoryId.hashCode ^ name.hashCode ^ description.hashCode ^ image.hashCode ^ slug.hashCode ^ products.hashCode;
 }
