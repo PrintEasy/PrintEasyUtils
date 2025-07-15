9a8e92aad3e304ec0dc5055b6667be9dabbdb27e
piyush <piyush72717272@gmail.com>
2025-07-15 09:41:11 +0530

    feat: add gift reward and promo headline models to exports

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -5,11 +5,14 @@ export 'commission_model.dart';
 export 'dialog_model.dart';
 export 'franchise_model.dart';
 export 'gallery_model.dart';
+export 'gift_reward_model.dart';
 export 'order/order.dart';
 export 'place_detail_model.dart';
 export 'prediction_model.dart';
 export 'print_image.dart';
 export 'product_model.dart';
+export 'promo_headline_model.dart';
 export 'repo_response.dart';
 export 'support/support.dart';
 export 'user_model.dart';
+
